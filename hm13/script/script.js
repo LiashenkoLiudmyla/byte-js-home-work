43e85ee02ab05b98331582aa2dd1a87b56a4c080
Liashenko <lyudmila.fox17@gmail.com>
2023-02-08 20:10:14 +0100

    2 var

--- hm13/script/script.js
+++ hm13/script/script.js
@@ -1,38 +1,92 @@
+//  I var ///////////////////////////////
 
-// const createTooltip = (button, text) => { //button-до чого привязуємо тултип з text
 
-  
-//   const tooltip = document.createElement('div')//створюємо елемент
-//   tooltip.classList.add('tooltip')// додаємо клас tooltip
-//   tooltip.innerText = text // якийс контент у тому тултипі
-//  //ми створили цей тултип, але його в HTML його ще немає тому апендимо
+//   /* цей код позначає картинки, для зручності розробки */
+//   let i = 1;
+//   for(let li of carousel.querySelectorAll('li')) {
+//     li.style.position = 'relative';
+//     li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+//     i++;
+//   }
 
-//   // апендимо відразу після створення, 
-//   // щоб отримати ширину та висоту елемента, щоб вирахувати координати
-//   document.body.append(tooltip);
- 
-//   let tooltipCoord = document.querySelector('.button')
+//   /* Конфігурація */
+//   let width = 250; // ширина зображення
+//   let count = 1; // видима кількість зображень
 
-//   // отримуємо координати елемента до якого кріпимо тултип
-//   const anchorElemCoords = tooltipCoord.getBoundingClientRect();
-//   // console.log(anchorElemCoords);
- 
-//   // вираховуємо верхні (вертикальні) координати тултипу
-//   // по такій формулі. 
-//   // Відступ кнопки від верхнього кордону екрану - 10 
-//   // відступ від кнопки до тултипу - висота тултипу
-//   const tooltipTop = anchorElemCoords.top - 10 - tooltip.clientHeight;
+//   let list = carousel.querySelector('ul');
+//   let listElems = carousel.querySelectorAll('li');
 
-//   // вираховуємо ліві (горизонтальні) координати тултипу
-//   // по такій формулі. 
-//   // Відступ кнопки від лівого кордону екрану +  
-//   // половина ширини кнопки +
-//   // половина ширини тултипу
-//   const tooltipLeft = anchorElemCoords.left +
-//   anchorElemCoords.width / 2 -
-//   tooltip.clientWidth / 2;
+//   let position = 0; // початкова позиція каруселі
 
-//   tooltip.style.top = `${tooltipTop}px`
-//   tooltip.style.left = `${tooltipLeft}px` 
-// }
-// createTooltip('.button', 'example text bla bla bla');
+//   carousel.querySelector('.prev').onclick = function() {
+//     // зсув ліворуч
+//     position += width * count;
+//     // останній зсув вліво може бути не на 3, а на 2 або 1 елемент
+//     position = Math.min(position, 0)
+//     list.style.marginLeft = position + 'px';
+//   };
+
+//   carousel.querySelector('.next').onclick = function() {
+//     // зсув праворуч
+//     position -= width * count;
+//     // останнє пересування вправо може бути не на 3, а на 2 або 1 елемент
+//     position = Math.max(position, -width * (listElems.length - count));
+//     list.style.marginLeft = position + 'px';
+//   };
+
+
+//  II var ////////////////////////////
+
+
+const slides = [...document.querySelectorAll('.slide')]; // повертає NodeList псевдомасив
+
+const prevBth = document.getElementById('bth-prev');
+
+const nextBth  = document.getElementById('bth-next');
+
+
+let currentSlideIndex = 0;
+
+//Сховаьт всі слайди окрім першого при повній загрузці DOM
+const hideSlidesOnLoad = () => {
+    const slidesToHide = slides.slice(1)
+
+    slidesToHide.forEach((slide) => {
+        slide.classList.add('hidden')
+    })
+
+}
+
+window.addEventListener('DOMContentLoaded', hideSlidesOnLoad)
+
+const showPrevSlide = () =>  {
+
+    slides[currentSlideIndex].classList.add('hidden')
+
+    if(currentSlideIndex === 0) {
+        slides[slides.length - 1].classList.remove('hidden')
+        currentSlideIndex = slides.length - 1
+     }else{
+        slides[currentSlideIndex -1].classList.remove('hidden')
+        currentSlideIndex--
+     }
+}
+
+const showNextSlide = () => {
+
+    slides[currentSlideIndex].classList.add('hidden')
+
+    if(currentSlideIndex === slides.length - 1){
+
+        slides[0].classList.remove('hidden')
+        currentSlideIndex = 0
+
+    }else{
+
+        slides[currentSlideIndex + 1].classList.remove('hidden')
+        currentSlideIndex++
+    }
+}
+
+prevBth.addEventListener('click', showPrevSlide)
+nextBth.addEventListener('click', showNextSlide)
