ace4a80bd4f22424be236ebba326a424559ca932
Liashenko <lyudmila.fox17@gmail.com>
2023-02-13 17:57:21 +0100

    HW 16

---
+++ hm16/script/script.js
@@ -0,0 +1,32 @@
+// Homework 16
+// Вашей задачей будет реализовать функционал приветсвия пользователя на сайте c помощью localStorage.
+
+// При первом входе на сайт просто показывать на экране приветствие Дооро пожаловать!
+// Если пользователь уже заходил на сайт (при первом визите), то к базовому приветсвию следует добавить строку Вы заходили раз: N. Где вместо N будет количесво посещений сайта пользователем. Обратите внимание, что первый визит так же считается.
+// Функционал должен работать при закрытии вкладки или бразура, обновлении страницы.
+
+const VISITS_CONNY_KEY = "visits"
+
+const renenderGreeting = (isInitial, visitsCount = 0) => {
+  const greetingElement = document.createElement("p")
+  const visitCountElement = document.createElement("p")
+  greetingElement.innerText = "Ласкаво просимо!"
+  visitCountElement.innerText = `Ви заходили ${visitsCount} разів`
+
+  document.body.append(greetingElement)
+
+  if(isInitial) {
+    document.body.append(visitCountElement)
+  }
+}
+
+if(localStorage.getItem(VISITS_CONNY_KEY)){
+  const visits = JSON.parse(localStorage.getItem("visits"))
+  const updatedVisits = visits + 1;
+  localStorage.setItem("visits", updatedVisits)
+
+  renenderGreeting(true, updatedVisits)
+  }else{
+    localStorage.setItem("visits", 1)
+    renenderGreeting(false)
+  }
