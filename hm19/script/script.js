aaa5afc3b0f1db1245f857ded98adae85707eb91
Liashenko <lyudmila.fox17@gmail.com>
2023-03-03 12:09:04 +0100

    errors

--- hm19/script/script.js
+++ hm19/script/script.js
@@ -14,13 +14,19 @@
 // https://restcountries.com
 // https://ipapi.co/
 
-const BASE_URL = "https://ipapi.co/json/"
-const BASE_URL_FLAG = "https://restcountries.com/v2/name"
+// const BASE_URL = "https://ipapi.co/json/"
+// const BASE_URL_FLAG = "https://restcountries.com/v2/name"
 const preloader = document.getElementById("preloader")
 
 const cardCont = document.getElementById("card-container")
 
-const showPreloader = (show) => {};
+const showPreloader = (show) => {
+    if(show) {
+        preloader.style.display = "block";
+    } else {
+        preloader.style.display = "none";
+    }
+};
 
 
 const renderFlag = (flag) => {
@@ -111,48 +117,179 @@ try {
 
 };
 
- getUserIpAdders (BASE_URL)
+//  getUserIpAdders (BASE_URL)
 
 
 
+// Задание 2   STAR WOR
+// С помощью сервиса swapi.dev запросите информацию о персонаже 
+//Звездных войн, а так же все фильмы, в которых он появлялся.
+
+// Для этого на странице должна быть форма, в которую 
+//пользователь может ввести id желаемого персонажа 
+//(доступны айди персонажей от 1 до 82).
+
+// После отправки формы, необходио отправить запрос по адресу 
+//https://swapi.dev/api/people/ID где вместо ID необходимо 
+//подставить значение, которое пользователь ввел в форму.
+
+// Далее, на странице необходимо отрендерить карточку с именем 
+//персонажа и кнопкой Фильмы.
+
+// При клике на кнопку в карточке, необходимо добавить 
+//в карточку с персонажем названия всех фильмов, в которых он появлялся. 
+
+//Информация о каждом фильме хранится по ссылкам, которые перечисленны 
+//в массиве по ключу films в ответе на предыдущий 
+//запрос (запрос на /people/ID).
+
+// Все запросы на фильмы необходимо отправлять с помощью 
+//метода Promise.all.
 
 
+const BASE_URL = "https://swapi.dev/api/people"
+const idForm = document.getElementById("form") // -> форма з id = "form"
+const input = document.getElementById("input") // -> input з id = "input"
+const contChar = document.getElementById("charakter-container") // -> div з id = "charakter-container"
+
+//обробка помилки 
+const handleErrors = async (response) => {
+    if(!response.ok){
+        let { error } = await response.json()
+        throw new Error(response.status)
+    }
+    return response
+};
 
 
+// додаємо фільми в карточку
+const renderFilmList = (arrFilms) => {
+
+    let nameFilms = arrFilms.map(film => film.title)
+
+    const filmNameCont = document.createElement("div")
+
+    for(let name of nameFilms){
+
+        const filmName = document.createElement("p")
+        filmName.innerText = name
+        filmName.classList.add("film-text")
+        filmNameCont.append(filmName)
+    }
+
+    contChar.firstElementChild.append(filmNameCont)
+   
+};
 
 
+// запит на фільми
+const handleButtonFilms = async ({ films }) => {
+
+    const fetchFilms = films.map((film) => fetch(film))
+
+// console.log("fetchFilms", fetchFilms) не консолиться жодна консоль
+
+    try {
+        const response = await Promise.all(fetchFilms)
+        const jsonResponses = responses.map((response) => response.json());
+        const result = await Promise.all(jsonResponses)
+
+        await renderFilmList(result)
+
+    } catch {
+        alert("Error")
+    }
+
+    
+};
 
 
+// рендеримо картку персонажа
+const renderCharacterCard = (char) => {
+    const { name } = char
+    const divChar = document.createElement("div")
+    divChar.classList.add("card")
+
+    const nameChar = document.createElement("p")
+    nameChar.style.marginBottom = "15px"
+    nameChar.innerText = name
+
+    const btnChar = document.createElement("button")
+    btnChar.classList.add("button")
+    btnChar.innerText = "FILMS"
+
+    divChar.append(nameChar, btnChar);
+    contChar.append(divChar);
+    
+    btnChar.addEventListener("click", (event) => {
+
+        if(!event.target.hasAttribute("isActive")){
+            event.target.setAttribute("isActive", "")
+            handleButtonFilms(char)
+        }else{
+            
+            event.target.removeAttribute("isActive")
+            event.target.nextElementSibling.remove() // не змогла подивитися по дом дереву  
+        }
+
+       
+     
+    })
+
+    
+};
 
 
+// отримуємо персонажа
+const getCharacter = async (id) => {
+
+    try {
+       const response = await handleErrors(
+        await fetch(`${BASE_URL}/${id}`)
+       ) 
+
+       const charakter = await response.json();
+
+       await renderCharacterCard(charakter)
+       
+    } catch (error) {
+
+        if(+error.message === 404){
+            alert("Нема персонажа з таким id ")
+        }else {
+            alert("Error")
+        }
+        
+    }
+    
+
+};
+// for (var i = 0; i < document.body.childNodes.length; i++) {
+//       alert( document.body.childNodes[i] ); // последовательно выведет дочерние элементы document.body:
+//     }
+
+// обробка форми 
+const handleIdForm = (event) => {
+ 
+    event.preventDefault()
+
+    const { value } = input
+
+    if (value === ""){
+        alert ("Введіть число")
+        return
+    }else{
+        getCharacter(value)
+    }
+     
+contChar.firstElementChild.remove()
+};
 
 
+// запит форми
+ idForm.addEventListener( "submit", handleIdForm)
+
+ const test = handleIdForm
+console.log('test', test)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Задание 2
-// С помощью сервиса swapi.dev запросите информацию о персонаже Звездных войн, а так же все фильмы, в которых он появлялся.
-
-// Для этого на странице должна быть форма, в которую пользователь может ввести id желаемого персонажа (доступны айди персонажей от 1 до 82).
-// После отправки формы, необходио отправить запрос по адресу https://swapi.dev/api/people/ID где вместо ID необходимо подставить значение, которое пользователь ввел в форму.
-// Далее, на странице необходимо отрендерить карточку с именем персонажа и кнопкой Фильмы.
-// При клике на кнопку в карточке, необходимо добавить в карточку с персонажем названия всех фильмов, в которых он появлялся. Информация о каждом фильме хранится по ссылкам, которые перечисленны в массиве по ключу films в ответе на предыдущий запрос (запрос на /people/ID).
-// Все запросы на фильмы необходимо отправлять с помощью метода Promise.all.
